44fcb72fa99c2afbdc2be4e80a48a8f3a642ffa6
Vetik Tsvetik <vetik32@yahoo.com>
2014-12-05 11:18:21 +0200

    bower's packages updates

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,14 +3,14 @@
 Emberdemo.setupForTesting();
 Emberdemo.injectTestHelpers();
 
-var inputSelector = '[name="newComment"]';
+var newCommentSelector = '[name="newComment"]';
 var commentsSelector = 'ul.comments li';
 var addCommentSelector = 'button.btn-primary';
 
 
-module('Comment', {
+module('New Comment', {
     setup: function () {
-        visit("/comments");
+        visit("/comments/new");
     },
     teardown: function () {
         Emberdemo.reset();
@@ -19,11 +19,10 @@ module('Comment', {
 
 test('an input for new comment is present', function () {
     andThen(function () {
-        equal(find(inputSelector).length, 1);
+        equal(find(newCommentSelector).length, 1);
         equal(find(addCommentSelector).length, 1);
     });
 });
-
 
 test('list of default comments', function () {
     andThen(function () {
@@ -31,16 +30,13 @@ test('list of default comments', function () {
     });
 });
 
-
 test('new comment is added', function () {
     var newComment = 'My new comment';
     equal(find(addCommentSelector).is(':disabled'), true);
-    fillIn(inputSelector, newComment);
+    fillIn(newCommentSelector, newComment);
     equal(find(addCommentSelector).is(':disabled'), false);
     click(addCommentSelector);
     andThen(function () {
-        equal(find(inputSelector).val(), '');
-        equal(find(addCommentSelector).is(':disabled'), true);
         equal(find(commentsSelector).length, 3);
         equal(find(commentsSelector).last().find('.comment').text(), newComment);
     });
